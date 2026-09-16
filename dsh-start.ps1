fafa109ff9f65d84d3a3b858c2a587897bb5d3f1
chensir5173 <chensir5173@163.com>
2026-09-16 10:55:45 +0800

    调整下启动的脚本，设置dsh-home环境变量进行隔离；设置bat/sh脚本的结尾换行符

--- dsh-start.ps1
+++ dsh-start.ps1
@@ -1,1 +1,15 @@
-node .\apps\cli\lib\bin.js web --no-open
+$cur_dir = $PSScriptRoot
+$env:DSH_HOME = Join-Path $cur_dir .dsh_home
+
+# 判断是否需要同步会话
+if(-not (Test-Path -Path $env:DSH_HOME/sessions -PathType Container)) {
+    if(Test-Path -Path ~/.dsh/sessions -PathType Container) {
+        Start-Process powershell -Verb RunAs -ArgumentList "-NoExit", "-Command", "New-Item -ItemType SymbolicLink -Path '$($ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("$env:DSH_HOME/sessions"))' -Target '$($ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("~/.dsh/sessions"))'"
+    }
+}
+
+if ($args.Count -eq 0) {
+    node .\apps\cli\lib\bin.js web --no-open --port 3081
+} else {
+    node .\apps\cli\lib\bin.js @args
+}
